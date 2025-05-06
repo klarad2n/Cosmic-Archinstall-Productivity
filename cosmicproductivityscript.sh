c03d70bae1928af388924530d4f20875d3eeb155
Klara d2n <c.berger89@outlook.de>
2025-05-06 09:24:34 +0000

    Update cosmicproductivityscript.sh

--- cosmicproductivityscript.sh
+++ cosmicproductivityscript.sh
@@ -3,4 +3,4 @@ pacman -S wayland pipewire networkmanager power-profiles-daemon cosmic-app-libra
 pacman -S cosmic-idle cosmic-launcher cosmic-notifications cosmic-osd cosmic-panel cosmic-player cosmic-randr cosmic-screenshot cosmic-session
 pacman -S cosmic-settings cosmic-settings-daemom cosmic-store cosmic-text-editor cosmic-wallpapers cosmic-workspaces xdg-desktop-portal-cosmic
 systemctl enable cosmic-greeter.service
-pacman -S openh264 firefox firefox-i18n-en-us libreoffice-fresh gimp gimp-help-en thunderbird thunderbird-i18n-en-us guvcview flathub kitty
+pacman -S openh264 firefox firefox-i18n-en-us libreoffice-fresh gimp gimp-help-en thunderbird thunderbird-i18n-en-us guvcview flatpak kitty
