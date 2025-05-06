eaf28796a9d30a7f67906a6b1db65cb075ebd959
Klara d2n <c.berger89@outlook.de>
2025-05-06 04:10:49 +0200

    Update cosmicproductivityscript.sh

--- cosmicproductivityscript.sh
+++ cosmicproductivityscript.sh
@@ -1,5 +1,5 @@
 pacman -Syu
-pacman -S wayland power-profiles-daemon cosmic-app-library cosmic-applets cosmic-bg cosmic-comp cosmic-files cosmic-greeter cosmic-icon-theme
+pacman -S wayland pipewire power-profiles-daemon cosmic-app-library cosmic-applets cosmic-bg cosmic-comp cosmic-files cosmic-greeter cosmic-icon-theme
 pacman -S cosmic-idle cosmic-launcher cosmic-notifications cosmic-osd cosmic-panel cosmic-player cosmic-randr cosmic-screenshot cosmic-session
 pacman -S cosmic-settings cosmic-settings-daemom cosmic-store cosmic-terminal cosmic-text-editor cosmic-wallpapers cosmic-workspaces xdg-desktop-portal-cosmic
 systemctl enable cosmic-greeter.service
